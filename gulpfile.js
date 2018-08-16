8fd367c6379dc3db44a81a9ae7de558ff8f171dd
aug126 <doat.augustin@gmail.com>
2018-08-16 05:32:02 +0200

    initialisation du projet ToDoList avec les dépendances gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
+// Static Server + watching scss/html files
+gulp.task('default', ['sass'], function () {
+
+  browserSync.init({
+    server: "./",
+    tunnel: "test"
+  });
+
+  gulp.watch("src/sass/*.sass", ['sass'])
+  // .on('change', browserSync.reload);
+  gulp.watch(["index.html", "public/html/*.html"]).on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+  return gulp.src("src/sass/*.sass")
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(rename('syle.css'))
+    .pipe(gulp.dest("public/css"))
+    .pipe(browserSync.stream());
+});
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+import 'addTask.js';
