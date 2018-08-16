a180937ec1af0f42bc29747f06e354019866a25b
aug126 <doat.augustin@gmail.com>
2018-08-17 01:53:54 +0200

    ajout d'une tache (li) en cliquant sur le boutton ajouter, mise en style de la tâche, importation de font-awesome, modification du fichier gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,16 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
+var shell = require('gulp-shell');
 
 // Static Server + watching scss/html files
 gulp.task('default', ['sass'], function () {
-
   browserSync.init({
     server: "./",
     tunnel: "test"
   });
-
-  gulp.watch("src/sass/*.sass", ['sass'])
-  // .on('change', browserSync.reload);
-  gulp.watch(["index.html", "public/html/*.html"]).on('change', browserSync.reload);
-  // gulp.watch(["./public/js/*.js"]).on("change", browserSync.reload)
+  gulp.watch(["src/sass/**/*.sass"]).on('change', browserSync.reload)
+  gulp.watch(["index.html", "public/html/**/*.html"]).on('change', browserSync.reload);
+  gulp.watch(["./public/js/**/*.js"]).on("change", browserSync.reload)
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-  return gulp.src("src/sass/*.sass")
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(rename('syle.css'))
-    .pipe(gulp.dest("public/css"))
-    .pipe(browserSync.stream());
-});
+gulp.task('sass', shell.task('sass --watch src/sass:public/css &'));
--- public/js/addTask.js
+++ public/js/addTask.js
@@ -4,7 +4,16 @@ var todoList = document.getElementById("list");
 
 var addTask = () => {
   let newTask = document.createElement('li');
-  newTask.innerText = inputTask.value;
+  newTask.innerHTML =
+    '<div class="bg-body">\
+      <span> ' + inputTask.value + ' </span>\
+      <div>\
+        <button class="btn m-1"><i class="fas fa-check-circle"></i></button>\
+        <button class="btn m-1"><i class="fas fa-edit"></i></button>\
+        <button class="btn m-1"><i class="fas fa-trash-alt"></i></button>\
+      </div>\
+      </div>'
+  console.log(newTask);
   todoList.insertBefore(newTask, todoList.firstElementChild);
   inputTask.value = "";
 };
